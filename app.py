5928cc507258355dcc468204f0c99ba651ddca15
Kiran Narayan <kiran.narayan71@gmail.com>
2024-02-20 23:01:34 +0530

    connected script and app

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
+from src.convert_text_to_query import ConvertTextToQuery 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        sentence = request.form['sentence']
-        return render_template('index.html', sentence=sentence)
+        query = request.form['sentence']
+        
+        query_obj = ConvertTextToQuery()
+        result = query_obj.main(query)
+
+        return render_template('index.html', sentence=result)
     return render_template('index.html')
 
 if __name__ == '__main__':
